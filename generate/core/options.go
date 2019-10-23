dd771aee480427e132699232ae60923353b9a3b5
Moritz Zimmer <moritzzimmer@users.noreply.github.com>
2019-10-23 14:38:48 +0200

    version bumps (#20)

--- generate/core/options.go
+++ generate/core/options.go
@@ -42,10 +42,10 @@ func DefaultOpts() *Options {
 		},
 		Aws: &Aws{Region: "eu-west-1"},
 		Terraform: &Terraform{
-			Version: "0.12.8",
+			Version: "0.12.12",
 			Module: &Module{
 				Source:  "spring-media/lambda/aws",
-				Version: "4.1.0",
+				Version: "4.1.3",
 			},
 		},
 	}
