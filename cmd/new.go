bba0f82ce3004d626aaed637336142f43a428681
Moritz Zimmer <moritz.zmmr@gmail.com>
2020-01-31 16:44:27 +0100

    added support for new kinesis event
    we need https://github.com/spring-media/terraform-aws-lambda/pull/49
    merged and released as v4.4.0 before

--- cmd/new.go
+++ cmd/new.go
@@ -78,7 +78,7 @@ func init() {
 	rootCmd.AddCommand(newCmd)
 
 	newCmd.Flags().BoolP("dry-run", "d", false, "dry run")
-	newCmd.Flags().StringP("event", "e", "cloudwatch-event", "event type triggering the Lambda function [cloudwatch-event, dynamodb, s3, sns]")
+	newCmd.Flags().StringP("event", "e", "cloudwatch-event", "event type triggering the Lambda function [cloudwatch-event, dynamodb, kinesis, s3, sns]")
 	newCmd.Flags().String("ci", "none", "ci provider config file to generate [none, travis]")
 	viper.BindPFlags(newCmd.Flags())
 }
